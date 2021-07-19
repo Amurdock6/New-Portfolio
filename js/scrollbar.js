4be769c1403d6b2986ce17b4ae9c768f06c49eb9
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-07-19 01:59:54 -0600

    Initial commit

---
+++ js/scrollbar.js
@@ -0,0 +1,6 @@
+window.onscroll = function () {
+    let windowScroll = document.documentElement.scrollTop;
+    let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollAmount = (windowScroll / windowHeight) * 100;
+    document.getElementById("mybar").style.width = scrollAmount + "%";
+  };
