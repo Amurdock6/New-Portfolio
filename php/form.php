cf84564b1adb25b60f8556e3d7605d372b32a7fe
Amurdock6 <77793000+Amurdock6@users.noreply.github.com>
2021-08-14 16:35:04 -0600

    Added contact form and some other small updates

---
+++ php/form.php
@@ -0,0 +1,14 @@
+<?php 
+   if (isset($_POST['submit'])){
+       $name = $_POST['name'];
+       $email = $_POST['email'];
+       $message = $_POST['message'];
+
+        $mailTo = "business@alexanderrmurdock.com";
+        $headers = "From: ".$email;
+        $txt = "Email From".$name.".\n\n".$message;
+
+       mail($mailTo, $txt, $headers);
+       header("Location: ../contact.html");
+   }
+?>
